91fbc7c827b26d35f769d74a5873e379858e3ec4
Munna -Dev <mkcodes.in@gmail.com>
2026-07-14 20:10:14 +0530

    In Backend added cors for frontend can communicate. in frontend creating endpoint file to all neccessory endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,10 +5,15 @@ import cookieParser from "cookie-parser";
 import courseRouter from "./routes/course.route";
 import categoryRouter from "./routes/category.route";
 import enrollRouter from "./routes/enroll.route";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 
 app.use("/auth", authRouter);
 app.use("/course", courseRouter);
