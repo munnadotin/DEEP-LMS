75bfde3e67b160d7e3793427fc7e34a00229b7b2
Munna -Dev <mkcodes.in@gmail.com>
2026-07-26 09:20:57 +0530

    Fixing env variable name

--- server/src/controllers/enroll.controllert.ts
+++ server/src/controllers/enroll.controllert.ts
@@ -70,22 +70,13 @@ const enrollCourse = async (req: Request, res: Response) => {
 // Verify course
 const verifyEnrollment = async (req: Request, res: Response) => {
     try {
-        const {
-            razorpay_order_id,
-            razorpay_payment_id,
-            razorpay_signature,
-        } = req.body;
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
-        const body =
-            razorpay_order_id + "|" + razorpay_payment_id;
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-        const expectedSignature = crypto
-            .createHmac("sha256", process.env.RAZORPAY_SECRET!)
-            .update(body.toString())
-            .digest("hex");
+        const expectedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET_KEY!).update(body.toString()).digest("hex");
 
         if (expectedSignature !== razorpay_signature) {
-
             return res.status(400).json({
                 success: false,
                 message: "Payment verification failed",
